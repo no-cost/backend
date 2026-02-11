8f241da5c89ab1197d98cf28795fd54f3bfd9046
Kevin Svitač <kevin@svit.ac>
2026-02-11 17:44:50 +0100

    add length validation to tag

--- utils/__init__.py
+++ utils/__init__.py
@@ -15,6 +15,10 @@ def random_string(*args, **kwargs) -> str:
 
 
 def validate_tag(tag: str) -> str:
+    if len(tag) > 32:
+        raise ValueError("Tag must be less than 32 characters.")
+    if len(tag) < 3:
+        raise ValueError("Tag must be at least 3 characters.")
     if not TAG_PATTERN.match(tag):
         raise ValueError("Tag must contain only letters, digits, and underscores.")
-    return tag.lower()
+    return tag
