eca48c08ff0664f753f5571dceae8071cc33f5ac
Kevin Svitač <kevin@svit.ac>
2026-02-12 17:32:59 +0100

    add comment about `python-dotenv` dependency

--- cli/__init__.py
+++ cli/__init__.py
@@ -4,8 +4,10 @@ from pathlib import Path
 # add backend root to sys.path so top-level modules (settings, etc.) are importable from CLI entrypoints
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-# TODO: load via source in bash profile on login instead?
-# would drop python-dotenv dependency
+# ugh unfortunately has to depend on python-dotenv for CLI scripts,
+# even despite systemd using EnvironmentFile
+# and cron jobs can't use something like .bashrc to load env vars
+# so python-dotenv covers all non-shell uses
 from dotenv import load_dotenv
 
 load_dotenv()
