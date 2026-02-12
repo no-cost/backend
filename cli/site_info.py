7eec54702666edad7ebd230027d2e6b868d23ae2
Kevin Svitač <kevin@svit.ac>
2026-02-12 17:41:17 +0100

    reorder info in `site_info` output to make it more comprehensive

--- cli/site_info.py
+++ cli/site_info.py
@@ -23,23 +23,26 @@ async def _main():
 
         cc = get_country_code(site.created_ip) if site.created_ip else None
 
-        print(f"tag:            {site.tag}")
-        print(f"site_type:      {site.site_type}")
-        print(f"hostname:       {site.hostname}")
+        print(f"tag:            {site.tag} ({site.site_type})")
         print(f"admin_email:    {site.admin_email}")
-        print(f"installed:      {site.installed_at or 'not yet (or failed)'}")
+        print(f"hostname:       {site.hostname}")
+        print()
         print(f"created_at:     {site.created_at}")
         print(f"created_ip:     {site.created_ip or 'N/A'}{f' ({cc})' if cc else ''}")
+        print(f"installed:      {site.installed_at or 'not yet (or failed)'}")
+        print()
+        print(f"last_login_at:  {site.last_login_at or 'never'}")
         print(
             f"last_login_ip:  {site.last_login_ip or 'N/A'}{f' ({cc})' if cc else ''}"
         )
-        print(f"last_login_at:  {site.last_login_at or 'never'}")
+        print()
         print(
             f"donor:          {f'yes ({site.donated_amount} EUR)' if site.is_donor() else 'no'}"
         )
         print(f"has_perks:      {'yes' if site.has_perks() else 'no'}")
 
         if site.removed_at:
+            print()
             print(f"removed_at:     {site.removed_at}")
             print(f"removed_ip:     {site.removed_ip or 'N/A'}")
             print(f"removal_reason: {site.removal_reason or 'N/A'}")
